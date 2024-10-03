9cc923e9809677f7dce7150fe15bd1b7d5673d6b
VictorCottar <victorcottarms@outlook.com>
2024-10-02 22:20:36 -0300

    Logout no profile

--- src/app/profile.tsx
+++ src/app/profile.tsx
@@ -7,19 +7,32 @@ export default function Profile() {
 
   return (
 
-
     <View className='flex-1 items-center font-archivo'>
 
-      <View className='flex flex-row justify-end items-center w-full h-24 border-b-2 bg-black'>
-        <Text className='text-3xl font-semibold text-white ml-5'>perfil do {username}</Text>
-        <TouchableOpacity
-          activeOpacity={1}
-          className="flex justify-center items-center h-16 w-16"
-          onPress={() => router.push({ pathname: '/todo', params: { username } })}
-        >
-          <AntDesign name="back" size={24} color="white" />
-        </TouchableOpacity>
+      <View className='flex flex-row items-end w-full h-24 border-b-2 bg-black'>
+        <View className="flex flex-row mt-8 justify-between items-end  ml-10 w-60">
+          <TouchableOpacity
+            className="flex justify-center items-start h-16 w-16 top-4 right-3"
+            onPress={() => router.push({ pathname: '/todo', params: { username } })}
+          >
+            <AntDesign name="back" size={24} color="white" />
+          </TouchableOpacity>
+          <Text className='text-3xl font-semibold text-white ml-1'>perfil do {username}</Text>
+        </View>
       </View>
+
+      <View className="flex justify-center items-center w-11/12 h-32 p-2 border-2 border-b-4 border-r-4 mt-10 rounded-sm shadow-shape">
+        <Text className="text-2xl font-medium ">parabéns!</Text>
+        <Text className="text-xl font-medium ">você já concluiu x tarefas</Text>
+      </View>
+
+      <TouchableOpacity
+          className='flex justify-center items-center border-2 border-r-4 border-b-4 h-12 w-11/12 p-2 rounded-sm shadow-shape mt-12'
+          onPress={() => router.push('/')}
+        >
+          <Text className='text-xl font-medium '>logout</Text>
+        </TouchableOpacity>
+
     </View>
   )
 }
--- src/app/todo.tsx
+++ src/app/todo.tsx
@@ -18,16 +18,16 @@ export default function Todo() {
         <Text className="text-xl font-semibold">qual o plano para hoje?</Text>
       </View>
 
-      <View className="flex justify-start items-center w-full h-2/3  mt-8 space-y-4">
-        <View className="flex justify-start items-start w-11/12 h-16  p-1 border-2 border-b-4 border-r-4">
+      <View className="flex justify-start items-center w-full h-2/3 mt-8 space-y-4">
+        <View className="flex justify-start items-start w-11/12 h-16 p-1 border-2 border-b-4 border-r-4 rounded-sm shadow-shape">
           <Text className="text-lg font-medium ml-3">terminar tela do App</Text>
         </View>
 
-        <View className="flex justify-start items-start w-11/12 h-16  p-1 border-2 border-b-4 border-r-4">
+        <View className="flex justify-start items-start w-11/12 h-16  p-1 border-2 border-b-4 border-r-4 rounded-sm shadow-shape">
           <Text className="text-lg font-medium ml-3">terminar tela do App</Text>
         </View>      
 
-        <View className="flex justify-start items-start w-11/12 h-16  p-1 border-2 border-b-4 border-r-4">
+        <View className="flex justify-start items-start w-11/12 h-16  p-1 border-2 border-b-4 border-r-4 rounded-sm shadow-shape">
           <Text className="text-lg font-medium ml-3">terminar tela do App</Text>
         </View>
       
@@ -36,21 +36,19 @@ export default function Todo() {
       <View className="w-full h-28 bg-black">
           <View className="flex flex-row justify-around items-start">
             <TouchableOpacity
-              activeOpacity={1}
               className="flex justify-center items-center h-16 w-16"
             >
               <FontAwesome5 name="list" size={24} color="white" />
             </TouchableOpacity>
             
             <TouchableOpacity
-              activeOpacity={1}
               className="flex justify-center items-center h-20 w-20 bg-white rounded-full border-4 bottom-5"
             >
               <Entypo name="plus" size={38} color="black" />
             </TouchableOpacity>
 
             <TouchableOpacity
-              activeOpacity={1}
+              
               className="flex justify-center items-center h-16 w-16"
               onPress={() => router.push({ pathname: '/profile', params: { username }})}
             >
